1cce3b32ad8d65710b80ce29a1f60e61b8c0de2f
Pavlenko Arsenii <ampavlenko@miem.hse.ru>
2022-11-15 16:56:24 +0300

    Introducing

---
+++ Start with correcr encoding.bat
@@ -0,0 +1,1 @@
+python main.py PYTHONIOENCODING=chcp1251
--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 #python 3.10
+
+# шифр простой замены
+# аффинный шифр
+# аффинный рекуррентный шифр
+
+from intro import introMS
+
+def start():
+    OperType, CrypType = introMS()
+    if OperType + CrypType != 0: CheckF(OperType, CrypType)
+    return None
+
+def CheckF(OperType, CrypType):
+    print(OperType, CrypType)
+    input('Нажмите Enter для выхода\n')
+    return None
+
+if __name__ == "__main__":
+	start()
